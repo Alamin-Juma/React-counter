78c6660628203858deffb1cab29b44aa5097e5f6
Alamin-Juma <alaminjuma96@gmail.com>
2020-11-30 21:00:22 -0800

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
+import React, {useState} from "react";
 import "./App.css";
 import Button from "./components/Button";
 
+
 function App() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  const incrementCount = (increment) => {
-    setCount(count + increment);
-  };
+    const incrementCount = (increment) => {
+        setCount(count + increment);
+    };
+    
 
-  return (
-    <div className="App">
-      <Button increment={1} onClickFunction={incrementCount} />
-      <Button increment={10} onClickFunction={incrementCount} />
-      <Button increment={100} onClickFunction={incrementCount} />
-      <Button increment={1000} onClickFunction={incrementCount} />
-      <span>{count}</span>
-    </div>
-  );
+    return (
+        <div className="App ">
+
+                <Button increment={10} onClickFunction={incrementCount}/>
+                <Button increment={100} onClickFunction={incrementCount}/>
+                <Button increment={1000} onClickFunction={incrementCount}/>
+                <span>{count}</span>
+            </div>
+    );
 }
 
 export default App;
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,16 @@
 import React from "react";
+import "../App.css";
 
 const Button = ({ increment, onClickFunction }) => {
   const handleClick = () => {
     onClickFunction(increment);
   };
-  return <button onClick={handleClick}>+{increment}</button>;
+  return (
+      <div className="Button ">
+        <button onClick={handleClick}>+{increment}</button>
+      </div>
+  );
+
 };
 
 export default Button;
